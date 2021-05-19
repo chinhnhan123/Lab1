4d91b878b106d29ac8a461d9244e362563d39ebb
chinhnhan123 <nhanchinh113@gmail.com>
2021-05-19 20:49:58 +0700

    Lab 1 advanced programing

---
+++ lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lab1
+{
+    public class SV
+    {
+        public int ID, Age;
+        public string Name;
+        public SV() { }
+        public SV(int id, string name,int age )
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+        }
+        public void invs() 
+        {
+            Console.WriteLine("ID = {0} , Name = {1} va Age = {2} ", ID , Name , Age);
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SV h1 = new SV(1, "nhan" , 20);
+            SV h2 = new SV(2, "minh", 20);
+            SV h3 = new SV(3, "khoa", 22);
+            h1.invs();
+            h2.invs();
+            h3.invs();
+        }
+    }
+}
